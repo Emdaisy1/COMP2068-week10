88653d31ab7e65a87cc3f80b058bd48cb33d7187
Emdaisy1 <hilborn_emmav@live.ca>
2015-11-19 15:41:55 -0500

    Added manual bootstrapping of angular

--- public/app.js
+++ public/app.js
@@ -1,2 +1,17 @@
-var app = angular.module("app", []);
+/// <reference path="../typings/jquery/jquery.d.ts" />
+/// <reference path="../typings/angularjs/angular.d.ts" />
+//var app:ng.IModule = angular.module("app", []);
+//Wait for document to load, before bootstrapping angular app
+var mainApplicationModuleName = "mean";
+///Declare angular module
+var mainApplicationModule = angular.module(mainApplicationModuleName, []);
+angular.element(document).ready(function () {
+    //Now manually bootstrap the app
+    angular.bootstrap(document, [mainApplicationModuleName]);
+});
+/*
+var app:ng.IModule = angular.module("app", []).controller("myController", ['$scope', function($scope) {
+
+}]);
+*/ 
 //# sourceMappingURL=app.js.map
--- public/app.ts
+++ public/app.ts
@@ -1,1 +1,19 @@
-var app = angular.module("app", []);
+/// <reference path="../typings/jquery/jquery.d.ts" />
+/// <reference path="../typings/angularjs/angular.d.ts" />
+
+//var app:ng.IModule = angular.module("app", []);
+
+//Wait for document to load, before bootstrapping angular app
+var mainApplicationModuleName:string = "mean";
+///Declare angular module
+var mainApplicationModule:ng.IModule = angular.module(mainApplicationModuleName, []);
+angular.element(document).ready(function () {
+	//Now manually bootstrap the app
+	angular.bootstrap(document, [mainApplicationModuleName]);
+});
+
+/*
+var app:ng.IModule = angular.module("app", []).controller("myController", ['$scope', function($scope) {
+
+}]);
+*/
